391f088e0e40cdf96b64e33cef6ff8909c9c8388
Noah Eckhardt <jawoll1909@gmail.com>
2025-10-09 00:42:57 +0200

    Added Egui

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin::default())
+        .add_plugins(WorldInspectorPlugin::default())
         .add_systems(Startup, setup)
         .add_systems(Update, update)
         .run();
